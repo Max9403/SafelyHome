1292249e0f3e779e739060ffe4e95f8a6a5bc3f6
Benjamin Claassen <BClaassen@live.com>
2014-04-26 14:07:42 +0200

    Added photo selection implementation

--- app/src/main/java/com/gdgcapetown/safelyhome/app/MainActivity.java
+++ app/src/main/java/com/gdgcapetown/safelyhome/app/MainActivity.java
@@ -1,12 +1,28 @@
 package com.gdgcapetown.safelyhome.app;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +30,60 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void getPhoto(View view) {
+
+        CharSequence colors[] = new CharSequence[] {"Take photo", "Pick from galary"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Take a photo or select an image");
+        builder.setItems(colors, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch(which){
+                    case 0:
+                        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                        }
+                        break;
+                    case 1:
+                        Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        startActivityForResult(i, 100);
+                        break;
+                }
+            }
+        });
+        builder.show();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch(requestCode) {
+            case 100:
+                if(resultCode == RESULT_OK){
+                    InputStream imageStream = null;
+                    try {
+                        Uri selectedImage = imageReturnedIntent.getData();
+                        imageStream = getContentResolver().openInputStream(selectedImage);
+                        Bitmap image = BitmapFactory.decodeStream(imageStream);
+                        ((ImageView) findViewById(R.id.photoOfSource)).setImageBitmap(image);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            case REQUEST_IMAGE_CAPTURE:
+                if(resultCode == RESULT_OK) {
+                    Bundle extras = imageReturnedIntent.getExtras();
+                    Bitmap image = (Bitmap) extras.get("data");
+                    ((ImageView) findViewById(R.id.photoOfSource)).setImageBitmap(image);
+                }
+                break;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
